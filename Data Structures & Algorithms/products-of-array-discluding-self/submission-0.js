c562bead27f1544fcbef410f2b91c6cb64c1e73f
adityagupta-u1 <87804608+adityagupta-u1@users.noreply.github.com>
2026-05-11 22:41:30 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const res = new Array(n);
+        const prefix = new Array(n);
+        const suffix = new Array(n);
+
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+
+        for(let i =1; i < n;i++){
+            prefix[i] = nums[i-1] * prefix[i-1]
+        }
+        for(let i = n-2; i >=0; i--){
+            suffix[i] = nums[i+1] * suffix[i+1];
+        }
+        for(let i = 0; i < n;i++){
+            res[i] = prefix[i] * suffix[i]
+        }
+        return res;
+    }
+}
